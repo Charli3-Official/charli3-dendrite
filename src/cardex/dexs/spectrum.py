d741752d968ff648450338805e0bd27f50e485de
Elder Millenial <eldermillenial@protonmail.com>
2023-12-18 11:50:41 +0100

    Fixed bug in Spectrum parsing

--- src/cardex/dexs/spectrum.py
+++ src/cardex/dexs/spectrum.py
@@ -142,7 +142,7 @@ class SpectrumCPPState(AbstractConstantProductPoolState):
             pool_nft = Assets(
                 **{key: value for key, value in values["pool_nft"].items()},
             )
-            if not pool_nft.endswith("nft"):
+            if not pool_nft.unit().endswith("nft"):
                 raise NotAPoolError("A pool must have one pool NFT token.")
 
         # Check for the pool nft
