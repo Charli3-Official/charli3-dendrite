e2d767cd8fd4f68ff0b56e182ffaf8622c1a0dd4
Elder Millenial <eldermillenial@protonmail.com>
2024-01-17 03:28:46 +0100

    Fixed model config

--- src/cardex/dataclasses/models.py
+++ src/cardex/dataclasses/models.py
@@ -11,7 +11,7 @@ from pydantic.alias_generators import to_camel
 
 
 class CardexBaseModel(BaseModel):
-    model_config = ConfigDict(alias_generators=to_camel, populate_by_name=True)
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
 
 
 class PoolSelectorType(Enum):
