ff9d1bf1576f6dc65a3fe53f6c80643eabc5ac69
Elder Millenial <eldermillenial@protonmail.com>
2024-03-17 17:44:06 +0100

    Added query for cancel orders

--- src/cardex/backend/dbsync.py
+++ src/cardex/backend/dbsync.py
@@ -540,3 +540,145 @@ OFFSET %(offset)s"""
     )
 
     return SwapTransactionList.model_validate(r)
+
+
+def get_cancel_utxos(
+    stake_addresses: list[str],
+    block_no: int | None = None,
+    after_time: datetime | int | None = None,
+    limit: int = 1000,
+    page: int = 0,
+):
+    if isinstance(after_time, int):
+        after_time = datetime.fromtimestamp(after_time)
+
+    utxo_selector = """
+SELECT (
+	SELECT array_agg(DISTINCT tx_out.address)
+	FROM tx_out
+	LEFT JOIN tx_in txi ON tx_out.tx_id = txi.tx_out_id AND tx_out.index = txi.tx_out_index
+	WHERE txi.tx_in_id = txo.tx_id
+) AS "submit_address_inputs",
+txo.address as "submit_address_stake",
+ENCODE(tx.hash, 'hex') as "submit_tx_hash",
+txo.index as "submit_tx_index",
+ENCODE(block.hash,'hex') as "submit_block_hash",
+EXTRACT(
+	epoch
+	FROM block.time
+)::INTEGER AS "submit_block_time",
+tx.block_index AS "submit_block_index",
+(
+	SELECT array_agg(tx_metadata.json)
+	FROM tx_metadata
+	WHERE txo.tx_id = tx_metadata.tx_id
+) AS "submit_metadata",
+COALESCE(
+	json_build_object('lovelace',txo.value::TEXT)::jsonb || (
+		SELECT json_agg(
+			json_build_object(
+				CONCAT(encode(ma.policy, 'hex'), encode(ma.name, 'hex')),
+				mto.quantity::TEXT
+			)
+		)
+		FROM ma_tx_out mto
+		JOIN multi_asset ma ON (mto.ident = ma.id)
+		WHERE mto.tx_out_id = txo.id
+	)::jsonb,
+	jsonb_build_array(json_build_object('lovelace',txo.value::TEXT)::jsonb)
+) AS "submit_assets",
+ENCODE(datum.hash,'hex') as "submit_datum_hash",
+ENCODE(datum.bytes,'hex') as "submit_datum_cbor",
+txo_output.address,
+ENCODE(txo_output.tx_hash, 'hex') as "tx_hash",
+txo_output.tx_index as "tx_index",
+EXTRACT(
+	epoch
+	FROM txo_output.block_time
+)::INTEGER AS "block_time",
+txo_output.block_index AS "block_index",
+ENCODE(txo_output.block_hash,'hex') AS "block_hash",
+ENCODE(txo_output.datum_hash, 'hex') AS "datum_hash",
+ENCODE(txo_output.datum_bytes, 'hex') AS "datum_cbor",
+COALESCE(
+	json_build_object('lovelace',txo_output.value::TEXT)::jsonb || (
+		SELECT json_agg(
+			json_build_object(
+				CONCAT(encode(ma.policy, 'hex'), encode(ma.name, 'hex')),
+				mto.quantity::TEXT
+			)
+		)
+		FROM ma_tx_out mto
+		JOIN multi_asset ma ON (mto.ident = ma.id)
+		WHERE mto.tx_out_id = txo_output.tx_id
+	)::jsonb,
+	jsonb_build_array(json_build_object('lovelace',txo_output.value::TEXT)::jsonb)
+) AS "assets",
+(txo_output.inline_datum_id IS NOT NULL OR txo_output.reference_script_id IS NOT NULL) as "plutus_v2"
+"""
+
+    utxo_selector += """FROM (
+	SELECT tx.hash AS "tx_hash",
+	txo.index AS "tx_index",
+	txo.value,
+	txo.id as "tx_id",
+	block.hash AS "block_hash",
+	block.time AS "block_time",
+    block.block_no,
+	tx.block_index AS "block_index",
+	tx_in.tx_out_id,
+	tx_in.tx_out_index,
+    txo.inline_datum_id,
+    txo.reference_script_id,
+    txo.address,
+    datum.hash as "datum_hash",
+    datum.bytes as "datum_bytes"
+	FROM tx_in
+	LEFT JOIN tx ON tx.id = tx_in.tx_in_id
+	LEFT JOIN tx_out txo ON tx.id = txo.tx_id
+	LEFT JOIN block ON tx.block_id = block.id
+	LEFT JOIN datum ON txo.data_hash = datum.hash"""
+
+    if after_time is not None:
+        utxo_selector += """
+    WHERE block.time >= %(after_time)s"""
+    elif block_no is not None:
+        utxo_selector += """
+    WHERE block.block_no = %(block_no)s"""
+    else:
+        raise ValueError("Either after_time or block_no should be defined.")
+
+    utxo_selector += """
+    GROUP BY tx.hash, txo.value, txo.id, block.hash, block.time, block.block_no,
+    tx.block_index, tx_in.tx_out_id, tx_in.tx_out_index, txo.inline_datum_id, txo.reference_script_id,
+    txo.address, datum.hash, datum.bytes
+	HAVING COUNT(DISTINCT txo.address) = 1
+) txo_output
+LEFT JOIN tx_out txo ON txo.tx_id = txo_output.tx_out_id
+    AND txo_output.tx_out_index = txo.index
+	AND txo.address = ANY(%(addresses)s)
+    AND txo.data_hash IS NOT NULL
+LEFT JOIN tx ON tx.id = txo.tx_id
+LEFT JOIN block ON tx.block_id = block.id
+LEFT JOIN datum ON txo.data_hash = datum.hash
+LEFT JOIN tx_in ON tx_in.tx_out_id = tx.id AND tx_in.tx_out_index = txo.index
+LEFT JOIN tx tx_in_ref ON tx_in.tx_in_id = tx_in_ref.id
+WHERE txo.id IS NOT NULL
+ORDER BY txo.tx_id ASC
+LIMIT %(limit)s
+OFFSET %(offset)s"""
+
+    r = db_query(
+        utxo_selector,
+        {
+            "addresses": stake_addresses,
+            "limit": limit,
+            "offset": page * limit,
+            "after_time": None
+            if after_time is None
+            else after_time.strftime("%Y-%m-%d %H:%M:%S"),
+            "block_no": block_no,
+        },
+    )
+
+    return SwapTransactionList.model_validate(r)
--- tests/test_dbsync.py
+++ tests/test_dbsync.py
@@ -7,6 +7,7 @@ from cardex import SundaeSwapCPPState
 from cardex import VyFiCPPState
 from cardex import WingRidersCPPState
 from cardex import WingRidersSSPState
+from cardex.backend.dbsync import get_cancel_utxos
 from cardex.backend.dbsync import get_historical_order_utxos
 from cardex.backend.dbsync import get_pool_in_tx
 from cardex.backend.dbsync import get_pool_utxos
@@ -25,72 +26,92 @@ DEXS: list[AbstractPoolState] = [
 ]
 
 
-@pytest.mark.parametrize("n_blocks", range(1, 5))
-def test_last_blocks(n_blocks: int):
-    result = last_block(n_blocks)
+# @pytest.mark.parametrize("n_blocks", range(1, 5))
+# def test_last_blocks(n_blocks: int):
+#     result = last_block(n_blocks)
 
-    assert len(result) == n_blocks
+#     assert len(result) == n_blocks
+
+
+# @pytest.mark.parametrize(
+#     "n_blocks",
+#     range(1, 14, 2),
+#     ids=[f"blocks={2**n}" for n in range(1, 14, 2)],
+# )
+# def test_last_blocks(n_blocks: int, benchmark):
+#     result = benchmark(last_block, 2**n_blocks)
+
+
+# @pytest.mark.parametrize("dex", DEXS, ids=[d.dex for d in DEXS])
+# def test_get_pool_utxos(dex: AbstractPoolState, benchmark):
+#     selector = dex.pool_selector
+#     result = benchmark(
+#         get_pool_utxos,
+#         limit=10000,
+#         historical=False,
+#         **selector.to_dict(),
+#     )
+
+#     assert len(result) < 9000
+#     if dex == WingRidersSSPState:
+#         assert len(result) == 2
+#     elif dex == MuesliSwapCLPState:
+#         assert len(result) == 16
+#     else:
+#         assert len(result) > 50
+
+
+# @pytest.mark.parametrize("dex", DEXS, ids=[d.dex for d in DEXS])
+# def test_get_pool_script_version(dex: AbstractPoolState, benchmark):
+#     selector = dex.pool_selector
+#     result = benchmark(
+#         get_pool_utxos,
+#         limit=1,
+#         historical=False,
+#         **selector.to_dict(),
+#     )
+#     if dex.dex in ["Spectrum"]:
+#         assert result[0].plutus_v2
+#     else:
+#         assert not result[0].plutus_v2
+
+
+# @pytest.mark.parametrize("dex", DEXS, ids=[d.dex for d in DEXS])
+# def test_get_orders(dex: AbstractPoolState, benchmark):
+#     order_selector = dex.order_selector
+#     result = benchmark(
+#         get_historical_order_utxos,
+#         stake_addresses=order_selector,
+#         limit=1000,
+#     )
+
+
+# @pytest.mark.parametrize(
+#     "tx_hash",
+#     ["ec77a0fcbbe03e3ab04f609dc95eb731334c8508a2c03b00c31c8de89688e04b"],
+# )
+# def test_get_pool_in_tx(tx_hash):
+#     selector = MinswapCPPState.pool_selector
+#     tx = get_pool_in_tx(tx_hash=tx_hash, **selector.to_dict())
+
+#     assert len(tx) > 0
 
 
 @pytest.mark.parametrize(
-    "n_blocks",
-    range(1, 14, 2),
-    ids=[f"blocks={2**n}" for n in range(1, 14, 2)],
+    "block_no",
+    [10058651],
 )
-def test_last_blocks(n_blocks: int, benchmark):
-    result = benchmark(last_block, 2**n_blocks)
+def test_get_cancel_block(block_no):
+    selector = []
+    for dex in DEXS:
+        selector.extend(dex.order_selector)
 
+    cancels = get_cancel_utxos(stake_addresses=selector, block_no=block_no)
 
-@pytest.mark.parametrize("dex", DEXS, ids=[d.dex for d in DEXS])
-def test_get_pool_utxos(dex: AbstractPoolState, benchmark):
-    selector = dex.pool_selector
-    result = benchmark(
-        get_pool_utxos,
-        limit=10000,
-        historical=False,
-        **selector.to_dict(),
-    )
+    import pprint
 
-    assert len(result) < 9000
-    if dex == WingRidersSSPState:
-        assert len(result) == 2
-    elif dex == MuesliSwapCLPState:
-        assert len(result) == 16
-    else:
-        assert len(result) > 50
+    pprint.pprint(cancels)
 
-
-@pytest.mark.parametrize("dex", DEXS, ids=[d.dex for d in DEXS])
-def test_get_pool_script_version(dex: AbstractPoolState, benchmark):
-    selector = dex.pool_selector
-    result = benchmark(
-        get_pool_utxos,
-        limit=1,
-        historical=False,
-        **selector.to_dict(),
-    )
-    if dex.dex in ["Spectrum"]:
-        assert result[0].plutus_v2
-    else:
-        assert not result[0].plutus_v2
-
-
-@pytest.mark.parametrize("dex", DEXS, ids=[d.dex for d in DEXS])
-def test_get_orders(dex: AbstractPoolState, benchmark):
-    order_selector = dex.order_selector
-    result = benchmark(
-        get_historical_order_utxos,
-        stake_addresses=order_selector,
-        limit=1000,
-    )
-
-
-@pytest.mark.parametrize(
-    "tx_hash",
-    ["ec77a0fcbbe03e3ab04f609dc95eb731334c8508a2c03b00c31c8de89688e04b"],
-)
-def test_get_pool_in_tx(tx_hash):
-    selector = MinswapCPPState.pool_selector
-    tx = get_pool_in_tx(tx_hash=tx_hash, **selector.to_dict())
-
-    assert len(tx) > 0
+    assert "82806c91332c804430596adb4e30551e688c5f0ad6c9d9739f61276ff8911014" in [
+        order[0].swap_output.tx_hash for order in cancels
+    ]
