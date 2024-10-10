64a8fa55459d7f49b0f23073d1a5b12ab2e191bf
Elder Millenial <eldermillenial@protonmail.com>
2024-10-10 11:26:02 -0400

    Updated MinswapV2 batcher fee

--- src/charli3_dendrite/dexs/amm/minswap.py
+++ src/charli3_dendrite/dexs/amm/minswap.py
@@ -892,7 +892,7 @@ class MinswapV2CPPState(AbstractConstantProductPoolState):
     """Minswap Constant Product Pool State."""
 
     fee: int | list[int] = [30, 30]
-    _batcher = Assets(lovelace=1000000)
+    _batcher = Assets(lovelace=2000000)
     _deposit = Assets(lovelace=2000000)
     _stake_address: ClassVar[Address] = [
         Address.from_primitive(
@@ -937,24 +937,6 @@ class MinswapV2CPPState(AbstractConstantProductPoolState):
     @classmethod
     def pool_datum_class(self) -> type[MinswapV2PoolDatum]:
         return MinswapV2PoolDatum
-
-    def batcher_fee(
-        self,
-        in_assets: Assets | None = None,
-        out_assets: Assets | None = None,
-        extra_assets: Assets | None = None,
-    ) -> Assets:
-        """Batcher fee.
-
-        For Minswap, the batcher fee decreases linearly from 2.0 ADA to 1.5 ADA as the
-        MIN in the input assets from 0 - 25,000 MIN.
-        """
-        MIN = "29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c64d494e"
-        if extra_assets is not None and MIN in extra_assets:
-            fee_reduction = min(extra_assets[MIN] // (2 * 10**5), 250000)
-        else:
-            fee_reduction = 0
-        return self._batcher - Assets(lovelace=fee_reduction)
 
     @property
     def pool_id(self) -> str:
